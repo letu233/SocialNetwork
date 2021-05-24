a1a48db31df3db2a609f060893aafc9c274c3171
letu233 <fadokerpa@gmail.com>
2021-05-24 21:12:26 +0700

    Tu: Final

--- app/src/main/java/com/example/socialnetwork/Fragment/NotifyFragment.java
+++ app/src/main/java/com/example/socialnetwork/Fragment/NotifyFragment.java
@@ -2,17 +2,35 @@ package com.example.socialnetwork.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.socialnetwork.Adapter.NotificationAdapter;
 import com.example.socialnetwork.R;
+import com.example.socialnetwork.model.Notification;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class NotifyFragment extends Fragment {
-
+    private RecyclerView recyclerView;
+    private NotificationAdapter notificationAdapter;
+    private List<Notification> list;
 //    public NotifyFragment() {
 //        // Required empty public constructor
 //    }
@@ -41,6 +59,38 @@ public class NotifyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notify, container, false);
+        View view =  inflater.inflate(R.layout.fragment_notify, container, false);
+        recyclerView = view.findViewById(R.id.recycle_notification);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        list = new ArrayList<>();
+        notificationAdapter = new NotificationAdapter(getContext(), list);
+        recyclerView.setAdapter(notificationAdapter);
+        readNotifications();
+        return view;
+    }
+
+    private void readNotifications(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Notifications")
+                .child(firebaseUser.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot data: snapshot.getChildren()){
+                    Notification notification = data.getValue(Notification.class);
+                    list.add(notification);
+                }
+                Collections.reverse(list);
+                notificationAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
